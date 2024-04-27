a9386d7abce0a69779d2d8224171fe219a67a449
Abhra00 <abhramondal9674@gmail.com>
2024-04-27 10:02:29 +0530

    systray class is added & picom is reconfigured

--- suckless-software/st-0.9.2/config.def.h
+++ suckless-software/st-0.9.2/config.def.h
@@ -6,7 +6,7 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 
-static char *font = "Monospace:pixelsize=10:antialias=true:autohint=true";
+static char *font = "mono:pixelsize=10:antialias=true:autohint=true";
 /* Spare fonts */
 static char *font2[] = {
 /*     "Inconsolata for Powerline:pixelsize=12:antialias=true:autohint=true", */
