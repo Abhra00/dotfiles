c867712ed9a58561ee3d4dab8a8b224995b1ebe5
Abhra <abhramondal9674@gmail.com>
2024-05-27 11:55:15 +0530

    Update config.def.h

--- suckless-software/dwm-6.5/config.def.h
+++ suckless-software/dwm-6.5/config.def.h
@@ -266,7 +266,7 @@ ResourcePref resources[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbordercolor, "-sf", selfgcolor, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbordercolor, "-sf", selfgcolor, "-p", "Run:", NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *pmenucmd[] = { "pmenu", NULL };
 
