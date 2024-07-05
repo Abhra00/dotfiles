6a6690e496b5e99e7890306a57fad40d9d313bb1
Abhra00 <abhramondal9674@gmail.com>
2024-07-06 05:12:15 +0530

    nvim config is updtaed

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,11 @@
-require("craftscat")
+-- bootstrap lazy.nvim, LazyVim and your plugins
+if vim.loader then
+  vim.loader.enable()
+end
 
+_G.dd = function(...)
+  require("util.debug").dump(...)
+end
+vim.print = _G.dd
+
+require("config.lazy")
--- nvim/lua/craftscat/init.lua
+++
@@ -1,3 +0,0 @@
-require("craftscat.set")
-require("craftscat.remap")
-require("craftscat.lazy_init")
