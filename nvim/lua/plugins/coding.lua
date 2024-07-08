e31fe071cbc507581bb9860cf79cd13ec100c2dd
Abhra <abhramondal9674@gmail.com>
2024-07-09 02:55:51 +0530

    Update coding.lua

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -88,7 +88,7 @@ return {
   },
 
   {
-    "nvim-cmp",
+    "hrsh7th/nvim-cmp",
     dependencies = { "hrsh7th/cmp-emoji" },
     opts = function(_, opts)
       table.insert(opts.sources, { name = "emoji" })
